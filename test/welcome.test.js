46ecb24a38ea4521d2ef35bec4d88b226518d12c
Anderson Pires <79539843+7700453250@users.noreply.github.com>
2024-05-19 13:26:17 -0300

    Tirar Erro da Esteira

--- test/welcome.test.js
+++ test/welcome.test.js
@@ -1,5 +1,5 @@
 const welcome = require('../src/welcome');
 
 test('A função de boas-vindas retorna a mensagem correta', () => {
-    expect(welcome()).toBe('Bem-vindo ao mundo! error');
+    expect(welcome()).toBe('Bem-vindo ao mundo!');
 });
