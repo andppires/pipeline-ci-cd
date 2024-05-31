43c3588388ef0d6abe57f62a39791e92fc5b7f13
Anderson Pires <79539843+7700453250@users.noreply.github.com>
2024-05-31 17:50:46 -0300

    Forçar Erro Esteira

--- test/welcome.test.js
+++ test/welcome.test.js
@@ -1,5 +1,5 @@
 const welcome = require('../src/welcome');
 
 test('A função de boas-vindas retorna a mensagem correta', () => {
-    expect(welcome()).toBe('Bem-vindo ao mundo!');
+    expect(welcome()).toBe('Bem-vindo ao mundo');
 });
